a5b5d129369df4208ddb170499b6251fe1b17f2b
Goulshat <76894691+Goulshat@users.noreply.github.com>
2023-07-30 23:32:03 +0500

    удалены комментарии

--- js/comments.js
+++ js/comments.js
@@ -1,14 +1,13 @@
 const loadButton = document.querySelector('.comments-loader');
 
 const renderComments = (comments, counter, container) => {
-  const fragment = document.createDocumentFragment(); // коробочка для комментов
-  // console.log("Counter: ", counter);
-  counter = (counter > comments.length) ? comments.length : counter; // что больше - шаг или все комментарии?
+  const fragment = document.createDocumentFragment();
+  counter = (counter > comments.length) ? comments.length : counter;
 
-  if(counter < comments.length) { // комментариев больше чем шаг загрузки
-    loadButton.classList.remove('hidden'); // да, показываем кнопку для подгрузки
+  if(counter < comments.length) {
+    loadButton.classList.remove('hidden');
   } else {
-    loadButton.classList.add('hidden'); // нет, скрываем кнопку за ненадобностью
+    loadButton.classList.add('hidden');
   }
 
   document.querySelector('.social__comment-count').textContent = `
@@ -19,7 +18,7 @@ const renderComments = (comments, counter, container) => {
 
   commentsSelected.forEach((comment) => {
     const newComment = document.querySelector('.social__comment').cloneNode(true);
-    const avatar = newComment.querySelector('.social__picture') ;
+    const avatar = newComment.querySelector('.social__picture');
     newComment.querySelector('.social__text').textContent = comment.message;
     avatar.src = comment.avatar;
     avatar.alt = comment.name;
